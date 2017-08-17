2076a94ffd7624184b1e61f7f90cc73630b88585
Rodrigo Kammer <rodrigo.kammer@gmail.com>
2017-08-16 21:23:20 -0700

    season

--- tables/season.sql
+++ tables/season.sql
@@ -2,6 +2,9 @@ CREATE TABLE season(
     id                               INTEGER        NOT NULL AUTO_INCREMENT,
     title                            VARCHAR(50)    NOT NULL,
     season_number                    INTEGER        NOT NULL,
+    media_company_id                 INTEGER        NOT NULL,
+    business_group_id                INTEGER        NOT NULL,
+    network_id                       INTEGER        NOT NULL,
     production_id                    INTEGER        NOT NULL,
     budget                           NUMERIC(15,2)  NOT NULL DEFAULT 0.00,
     writing_start_date               DATE,
@@ -20,6 +23,9 @@ CREATE TABLE season(
     updated                          TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     updated_by                       VARCHAR(30),
     status                           VARCHAR(30),
-    CONSTRAINT pk_season_id          PRIMARY KEY (id),
-    CONSTRAINT fk_season_production  FOREIGN KEY (production_id) REFERENCES production (id)
+    CONSTRAINT pk_season_id             PRIMARY KEY (id),
+    CONSTRAINT fk_season_production     FOREIGN KEY (production_id)     REFERENCES production     (id),
+    CONSTRAINT fk_season_media_company  FOREIGN KEY (media_company_id)  REFERENCES company        (id),
+    CONSTRAINT fk_season_business_group FOREIGN KEY (business_group_id) REFERENCES business_group (id),
+    CONSTRAINT fk_network_id            FOREIGN KEY (network_id)        REFERENCES network        (id)
 ) ENGINE = InnoDB;
