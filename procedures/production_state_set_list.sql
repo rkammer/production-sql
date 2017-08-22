fb0bad0503948cf12938083aeed36843183d8691
Rodrigo Kammer <rkammer@missionpro.com>
2017-08-22 11:26:32 -0700

    avoid error on existing stage for a production

--- procedures/production_state_set_list.sql
+++ procedures/production_state_set_list.sql
@@ -4,31 +4,39 @@ CREATE PROCEDURE production_state_set_list(
     IN production_state_state_code       CHAR(2),
     IN production_state_created_by       VARCHAR(30),
     IN production_state_updated_by       VARCHAR(30),
-    IN production_state_status           VARCHAR(30),
-    OUT return_value                     INTEGER
+    IN production_state_status           VARCHAR(30)
 )
 BEGIN
     DECLARE ROW_EXISTS INTEGER;
 
-    INSERT INTO production_state
-    (
-        production_id,
-        state_code,
-        created_by,
-        updated_by,
-        status
-    )
-    VALUES
-    (
-        production_state_production_id,
-        production_state_state_code,
-        production_state_created_by,
-        production_state_updated_by,
-        'CREATED'
-    );
+    SELECT COUNT(*)
+      INTO ROW_EXISTS
+      FROM production_state
+     WHERE production_id = production_state_production_id
+       AND state_code    = production_state_state_code;
 
-    SET return_value = 0;
+    IF (ROW_EXISTS = 0) THEN
 
-     COMMIT;
+        INSERT INTO production_state
+        (
+            production_id,
+            state_code,
+            created_by,
+            updated_by,
+            status
+        )
+        VALUES
+        (
+            production_state_production_id,
+            production_state_state_code,
+            production_state_created_by,
+            production_state_updated_by,
+            'CREATED'
+        );
+
+        COMMIT;
+
+    END IF;
+
 END //
 DELIMITER ;
